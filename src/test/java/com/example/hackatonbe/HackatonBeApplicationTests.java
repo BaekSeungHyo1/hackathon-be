995d2aefdf5c639d5117522575160c88bcd6fdf8
BaekSeungHyo1 <shback0926@naver.com>
2026-07-22 14:39:48 +0900

    :tada: Start: hackaton-be 초기 세팅

---
+++ src/test/java/com/example/hackatonbe/HackatonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackatonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
